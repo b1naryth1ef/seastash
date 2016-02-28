df39b2130fe46060d369acbd54f2a845a7dc84cd
Andrei <b1naryth1ef@gmail.com>
2016-02-28 05:06:45 -0600

    Cleanup lua_debug_stack

--- src/include/global.h
+++ src/include/global.h
@@ -59,31 +59,30 @@ static bool lua_table_get(lua_State* L, char* key) {
   return false;
 }
 
-static void lua_debug_stack(lua_State* l)
-{
-    int i;
-    int top = lua_gettop(l);
+static void lua_debug_stack(lua_State* l) {
+  int i;
+  int top = lua_gettop(l);
 
-    printf("total in stack %d\n",top);
+  printf("total in stack %d\n",top);
 
-    for (i = 1; i <= top; i++)
-    {  /* repeat for each level */
-        int t = lua_type(l, i);
-        printf("  ");  /* put a separator */
-        switch (t) {
-            case LUA_TSTRING:  /* strings */
-                printf("string: '%s'\n", lua_tostring(l, i));
-                break;
-            case LUA_TBOOLEAN:  /* booleans */
-                printf("boolean %s\n",lua_toboolean(l, i) ? "true" : "false");
-                break;
-            case LUA_TNUMBER:  /* numbers */
-                printf("number: %g\n", lua_tonumber(l, i));
-                break;
-            default:  /* other values */
-                printf("%s\n", lua_typename(l, t));
-                break;
-        }
+  for (i = 1; i <= top; i++) {
+    int t = lua_type(l, i);
+    printf("  ");
+    switch (t) {
+      case LUA_TSTRING:
+        printf("string: '%s'\n", lua_tostring(l, i));
+        break;
+      case LUA_TBOOLEAN:
+        printf("boolean %s\n",lua_toboolean(l, i) ? "true" : "false");
+        break;
+      case LUA_TNUMBER:
+        printf("number: %g\n", lua_tonumber(l, i));
+        break;
+      default:
+        printf("%s\n", lua_typename(l, t));
+        break;
     }
-    printf("\n");  /* end the listing */
+  }
+
+  printf("\n");
 }
